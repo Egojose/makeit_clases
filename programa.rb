2fe8d5f121f3b8ac85b22ed40a116990269e4435
Jose <josemicrobiologo@gmail.com>
2018-07-17 19:19:24 -0500

    se crean 2 archivos en ruby

---
+++ programa.rb
@@ -0,0 +1,6 @@
+print "Ingresa tu nombre"
+
+name = gets.chomp
+
+puts "Hola #{name}"
+
---
+++ programa2.rb
@@ -0,0 +1,9 @@
+print "Escribe un numero"
+num1 = gets.chomp.to_i
+
+print "escribe otro"
+num2 = gets.chomp.to_i
+
+puts "la suma es igual a = #{num1 + num2}"
+
+
